83424a9ee0efbaaf5be56584fe48ee09aeae881a
Mikita Herasiutsin <mikita.herasiutsin@gmail.com>
2021-06-02 22:09:35 +0300

    Deploy to Heroku

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Currency conversion application
 
+The application is deployed with using Heroku.
+You can check functionality online by sending request via deployed [Swagger](https://nikita-currency-conversion.herokuapp.com/swagger-ui/#/)
+
+
 ### Local build
 
 `./gradlew clean bootRun`
 
-* [Then just open Swagger to test the endpoint](http://localhost:8080/swagger-ui/#/currency-conversion-controller/validateAddressUsingPOST)
+[Then just open Swagger to test the endpoint](http://localhost:8080/swagger-ui/#/currency-conversion-controller/validateAddressUsingPOST)
--- src/main/java/com/nherasiutsin/currencyconversion/CurrencyConversionController.java
+++ src/main/java/com/nherasiutsin/currencyconversion/CurrencyConversionController.java
@@ -1,10 +1,12 @@
 package com.nherasiutsin.currencyconversion;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+import springfox.documentation.annotations.ApiIgnore;
 
 import javax.validation.Valid;
 import java.math.BigDecimal;
@@ -12,6 +14,12 @@ import java.math.BigDecimal;
 @RestController
 @RequestMapping("/currency")
 public class CurrencyConversionController {
+
+    @ApiIgnore
+    @GetMapping("/")
+    public Mono<String> index() {
+        return Mono.just("index");
+    }
 
     @PostMapping("/convert")
     public Mono<ConversionResponseDTO> validateAddress(@RequestBody @Valid ConversionRequestDTO request) {
